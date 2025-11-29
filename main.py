ae9f8cd452ef36a91e6ba76aa8cf3a4263fed530
1151Kacka <1151@student.itgymnazium.cz>
2025-11-29 22:46:35 +0100

    2

--- main.py
+++ main.py
@@ -1,28 +1,98 @@
-print("Zadej uživatelské jméno a heslo.")
+USERS = {"bob": "123",
+         "ann": "pass123",
+         "mike": "password123",
+         "liz": "pass123"
+         }
+
 TEXTS = [
-'''Situated about 10 miles west of Kemmerer,
-Fossil Butte is a ruggedly impressive
-topographic feature that rises sharply
-some 1000 feet above Twin Creek Valley
-to an elevation of more than 7500 feet
-above sea level. The butte is located just
-north of US 30 and the Union Pacific Railroad,
-which traverse the valley.''',
-'''At the base of Fossil Butte are the bright
-red, purple, yellow and gray beds of the Wasatch
-Formation. Eroded portions of these horizontal
-beds slope gradually upward from the valley floor
-and steepen abruptly. Overlying them and extending
-to the top of the butte are the much steeper
-buff-to-white beds of the Green River Formation,
-which are about 300 feet thick.''',
-'''The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.'''
+    '''Situated about 10 miles west of Kemmerer,
+    Fossil Butte is a ruggedly impressive
+    topographic feature that rises sharply
+    some 1000 feet above Twin Creek Valley
+    to an elevation of more than 7500 feet
+    above sea level. The butte is located just
+    north of US 30 and the Union Pacific Railroad,
+    which traverse the valley.''',
+    '''At the base of Fossil Butte are the bright
+    red, purple, yellow and gray beds of the Wasatch
+    Formation. Eroded portions of these horizontal
+    beds slope gradually upward from the valley floor
+    and steepen abruptly. Overlying them and extending
+    to the top of the butte are the much steeper
+    buff-to-white beds of the Green River Formation,
+    which are about 300 feet thick.''',
+    '''The monument contains 8198 acres and protects
+    a portion of the largest deposit of freshwater fish
+    fossils in the world. The richest fossil fish deposits
+    are found in multiple limestone layers, which lie some
+    100 feet below the top of the butte. The fossils
+    represent several varieties of perch, as well as
+    other freshwater genera and herring similar to those
+    in modern oceans. Other fish such as paddlefish,
+    garpike and stingray are also present.'''
 ]
+
+def main():
+    # přihlášení.
+    if not logIn():
+        return
+    #Výběr textu.
+    selected_text = selectText()
+    # Analýza 
+    analyze_text(selected_text)
+
+def logIn():
+    username = input("username: ")  
+    password = input("password: ")  
+    print("----------------------------------------")  
+    if USERS.get(username) == password:
+        print(f"Welcome to the app, {username}")
+        print(f"We have {len(TEXTS)} texts to be analyzed.") 
+        print("----------------------------------------")  
+        return True 
+    else:
+        print("unregistered user, terminating the program..") 
+        return False
+
+def selectText():
+    selection = input(f"Enter a number btw. 1 and {len(TEXTS)} to select: ")
+    if not selection.isdigit():
+        print("Invalid input. Please enter a number. Terminating.") 
+        return False
+    index = int(selection) - 1
+    if index not in range(len(TEXTS)):
+        print(f"Invalid text number. Select between 1 and {len(TEXTS)}. Terminating.")
+        return False 
+    return TEXTS[index]
+
+def analyze_text(text):
+    text = TEXTS(selectText)
+    words = [word for word in text.split() if word]
+    titlecase_count = 0
+    uppercase_count = 0
+    lowercase_count = 0
+    numeric_count = 0
+    numeric_sum = 0
+    word_lengths = 0
+
+    for word in words: 
+        if word.isdigit():
+            numeric_count += 1  
+            numeric_sum += int(word)  
+        elif word.istitle():
+            titlecase_count += 1
+        elif word.isupper():
+            uppercase_count += 1
+        elif word.islower():
+            lowercase_count += 1
+        word_lengths[len(word)] += 1
+    print("----------------------------------------")
+    print(f"There are {len(words)} words in the selected text.")
+    print(f"There are {titlecase_count} titlecase words.")
+    print(f"There are {uppercase_count} uppercase words.")
+    print(f"There are {lowercase_count} lowercase words.")
+    print(f"There are {numeric_count} numeric strings.")
+    print(f"The sum of all the numbers {numeric_sum}")
+    print("----------------------------------------")
+    print(word_lengths)
+  
